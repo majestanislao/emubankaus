1b7cb6d155ec23a3dda96a7b889403a59b217bc1
majestanislao <70639850+majestanislao@users.noreply.github.com>
2020-09-02 21:44:57 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emu Bank Australia</title>
+    <link type="text/css" href="./css/style.css" rel="stylesheet"/>
+</head>
+
+<img src="images/emu.png" alt='Official logo' width='300px' height='100px'></a>
+
+
+<body>
+<div class="navbar">
+  	<a href="#">About Us</a>
+	<a href="#">Banking</a>
+  	<a href="#">Transactions</a>
+	<a href="#">Pay bills</a>
+	<a href="#">Logout</a>
+  	<a href="#">Account settings</a>
+	<a href="#">Customer Support</a>
+	<a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+	<a href="#">Logout</a>
+
+</div>
+	
+</body>
+</html>
