6dc4615b413fdd086d4f9b583479628e423f120f
Sujan20 <70643083+Sujan20@users.noreply.github.com>
2020-09-11 18:47:28 +1000

    Update details.php

--- details.php
+++ details.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Deposit/Withrawal</title>
-    <link type="text/css" href="./css/account.css" rel="stylesheet"/>
+    <link type="text/css" href="./css/details.css" rel="stylesheet"/>
 </head>
 <body>
     <header>
