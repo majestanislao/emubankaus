14eeee448d187987a16bf5a08e831fb19cad429b
majestanislao <70639850+majestanislao@users.noreply.github.com>
2020-09-17 00:17:43 +1000

    editing the whole thing

--- withdraw - Copy.php
+++ withdraw - Copy.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Withdraw Cash</title>
 	<link type="text/css" href="./css/withdraw.css" rel="stylesheet"/>
-	<link rel="stylesheet" href="./css/styles.css" rel="stylesheet"/>
+
 </head>
 <body>
     <header>
         <div class "emu">
-		<a href="index.html"><img class="logo" src="/Images/emu.png" alt="logo"></a>
+		<a href="index.html"><img class="logo" src="Images/emu.png" alt="logo"></a>
 		</div>
 		<nav>
              <ul class="nav-links">
@@ -20,46 +20,63 @@
                 <li><a href="deposit.php">Deposit Cash</a></li>
 				<li><a href="withdraw.php">Withraw Cash</a></li>
                 <li><a href="faqs.php">FAQs</a></li>
-				<li><a class= "logout" href="logout.php"><button>Log Out</button></a></li>
+				<li><a class= "logout" href="logout.html">Log Out</a></li>
 =======
                 <li><a href="details.html">Account Details</a></li>
                 <li><a href="transfer.html">Transfer Money</a></li>
                 <li><a href="deposit.html">Deposit Cash</a></li>
 				<li><a href="withdraw.html">Withraw Cash</a></li>
-				<li><a href="cardsettings.html">Card Settings</a></li>
                 <li><a href="faqs.html">FAQs</a></li>
 				<li><a class= "logout" href="logout.html"><button>Log Out</button></a></li>
 >>>>>>> c67adb61a070ae0a9181035057cd23c2f826d9f2:withdraw.html
             </ul>
         </nav>
     </header>
-		<form action="/action_page.php" class="container">
-        <h3>Withdraw Cash</h3>
 	
-		<center><select id="from" name="from">
-    	<option value="select">Select an account : </option>
-     	<option value="daily">Daily Savings 012345</option>
-      	<option value="Growth">GrowthSavings 3122345</option>
-		</select>
-
+		<div class="container">
+		<div style="text-align:center">
+		<h2>Withdraw Cash</h2>
+		</div>
+		<div class="row">
+		<div class="column">
+		<img src="Images/deposit.png" style="width:90%">
+		</div>
+	
+		<div class="column">
+		<form action="/action_page.php">
+      
+        <label for="From">Withdraw Cash to</label>
+        <select id="From" name="From">
+        <option value="select">Select an account : </option>
+		<option value="daily">Daily Savings 012345</option>
+		<option value="Growth">GrowthSavings 3122345</option>
+        </select>
+      
 		<label for="amount">Amount</label>
 		<input type="text" placeholder="Enter Amount" id="amount" name="amount" required>
-        
+			
 		<label for="desc">Description</label>
-		<input type="text"  id="desc" name="desc" placeholder="Optional">
+		<input type="text"  id="desc" name="desc" placeholder="Optional">    
+
 
 		<input type="Withdraw" value="Withdraw">
 		<input type="Cancel" value="Cancel">
+		
+     </form>
+    </div>
+  </div>
+</div>
 
-
-		<h4>Connect with us</h4>
-		<a href= "http://www.facebook.com"><img src="FONTAWSOME\facebook-square.svg" width="30"></a>
-		<a href= "http://wwww.twitter.com"><img src="FONTAWSOME\twitter-square.svg" width="30"></a>
-		<a href= "http://wwww.instagram.com"><img src="FONTAWSOME\instagram.svg" width="30"></a>
+		<br>
+		<br>
+		<center><h4>Connect with us</h4>
+		<a href= "http://www.facebook.com"><img src="FONTAWSOME\facebook-square.svg" width="50"></a>
+		<a href= "http://wwww.twitter.com"><img src="FONTAWSOME\twitter-square.svg" width="50"></a>
+		<a href= "http://wwww.instagram.com"><img src="FONTAWSOME\instagram.svg" width="50"></a>
+		<br>
 		<br>
 		&copy; Copyright 2020. All Rights Reserved.<br>
 		<a href="mailto:emubankaustralia.com">emubankaustralia@gmail.com</a></center>
  
-</form>
 </body>
 </html>
